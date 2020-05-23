64285d34b6717c7c0c95384ba93ab5092287cfee
Povilas Korop <povilask007@gmail.com>
2020-05-23 09:42:15 +0300

    Remove user's permission and middleware to check admin

---
+++ app/Http/Middleware/IsAdminMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!auth()->user()->is_admin) {
+            return redirect()->route('user.home');
+        }
+
+        return $next($request);
+    }
+}
